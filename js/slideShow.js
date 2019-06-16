a7b533814911b77312b3e1ada83b77ffd3ae4c13
Tri Luong <lxt0094@gmail.com>
2019-06-16 15:21:54 +0700

    Add slideShow for project.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

---
+++ js/slideShow.js
@@ -0,0 +1,32 @@
+var slideIndex = 0;
+var slidesArr = document.getElementsByClassName("slide");
+var maxslideIndex = slidesArr.length;
+var dots = document.getElementsByClassName("dot");
+
+slidesArr[0].style.display = "block";
+dots[0].className += " active";
+function plusSlides(n) {
+  showSlide((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlide(n);
+}
+
+function showSlide(index) {
+  if (index >= maxslideIndex) {
+    index = 0;
+  } else if (index < 0) {
+    index = maxslideIndex;
+  }
+
+  slideIndex = index;
+
+  for (i = 0; i < maxslideIndex; i++) {
+    slidesArr[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slidesArr[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
