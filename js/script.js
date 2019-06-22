7b51041b84f612109227db77bb1521fcea8fd5b4
Tri Luong <lxt0094@gmail.com>
2019-06-22 15:13:42 +0700

    Add productDetail to project.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// window.onload = function() {
+//   var sportLeftHeight = document.getElementsByClassName("sport-left")[0]
+//     .clientHeight;
+
+//   var showAllHeight =
+//     document.getElementsByClassName("show-all")[0].clientHeight +
+//     document.getElementsByClassName("show-all")[0].getComputedStyle.marginTop;
+//   var sportMenusArr = document.getElementsByClassName("sport-menus");
+//   sportMenusArr[0].style.height = sportLeftHeight - showAllHeight + "px";
+// };
